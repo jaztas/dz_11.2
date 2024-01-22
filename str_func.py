ff124038e0fd44326424c9d610e34edb9286fd26
Zotov Stanislav <zotovstanislav@mail.ru>
2024-01-22 12:39:26 +0300

    add capitalize function

--- str_func.py
+++ str_func.py
@@ -1,5 +1,14 @@
 def upper_string(a: str):
+	"""функция переводит строку в верхний регистр"""
 	return print(a.upper())
 
 
-# upper_string('salam')
+# upper_string()
+
+def capitalize_string():
+	"""функция переводит первую букву строки в верхний регистр"""
+	a = str(input("Enter word: "))
+	return print(a.capitalize())
+
+
+# capitalize_string()
